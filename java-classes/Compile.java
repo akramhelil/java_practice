85790ecc1f9a08ddc000375aba7f63a9a907e213
akramhelil <akramhelil@gmail.com>
2019-10-11 17:29:56 -0400

    variables

--- java-classes/Compile.java
+++ java-classes/Compile.java
@@ -2,8 +2,12 @@ public class Compile {
 
     public static void main(String[] args) {
 
-
-
+        // Variables
+        int number = 250;
+        double decimalNumbers = 2.3456;
+        boolean yesOrNo = true;
+        char singleLetter = 'A';
+        String sentenceString = "This is a sentence String";
 
         // commit 
         /* 
@@ -13,6 +17,11 @@ public class Compile {
         T
         I Line commit 
         */
+
         System.out.println("Hello");
+        System.out.println(number);
+        System.out.println(decimalNumbers);
+        System.out.println(yesOrNo);
+        System.out.println(sentenceString);
     }
 }
