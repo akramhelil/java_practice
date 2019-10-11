7b01bd5e83288cd39cf799b76e03fa3c7f72d518
akramhelil <akramhelil@gmail.com>
2019-10-11 17:11:21 -0400

    Multi line

--- java-classes/Compile.java
+++ java-classes/Compile.java
@@ -1,6 +1,18 @@
 public class Compile {
 
     public static void main(String[] args) {
+
+
+
+
+        // commit 
+        /* 
+        M
+        U
+        L
+        T
+        I Line commit 
+        */
         System.out.println("Hello");
     }
 }
