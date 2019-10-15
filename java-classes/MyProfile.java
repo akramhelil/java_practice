4663df72ca5445f8196fbc16e2acd2690d1d673a
akramhelil <akramhelil@gmail.com>
2019-10-15 12:28:32 -0400

    MyProfile

---
+++ java-classes/MyProfile.java
@@ -0,0 +1,12 @@
+public class MyProfile {
+
+    public static void main(String[] args) {
+        String name = "Akram";
+        int age = 30;
+        double desiredSalary = 90.5;
+        char gender = 'm';
+        boolean lookingForJob = true;
+
+        System.out.println("My name is " + name + " I am " + age + " years old " + " Looking For job: " + lookingForJob + " my desired salay is: " + desiredSalary);
+    }
+}
